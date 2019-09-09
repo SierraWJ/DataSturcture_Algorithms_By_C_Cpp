cc4767434755db79be8adf36abdfd7cbf4520cfe
Wiskey_Juliet <36991763+swj1265@users.noreply.github.com>
2019-09-09 15:19:50 +0900

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -2,28 +2,26 @@
 #include <stdlib.h>
 #include <windows.h>
 
-int staticNUM;
-
-void Print_List(int *List, int n){
+void Print_Array(int *Array, int n){
 	int i;
 	for(i = 0; i < n; i++){
-		printf("%d ",List[i]);
+		printf("%d ",Array[i]);
 	}
 	printf("\n");
 }
-void Print_Partial_List(int *List, int a, int b){
+void Print_Partial_Array(int *Array, int a, int b){
     int i;
     for(i = a; i < b; i++){
-        printf("%d ",List[i]);
+        printf("%d ",Array[i]);
     }
     printf("\n");
 }
-void Input_List(int *List, int N, int *n){
+void Input_Array(int *Array, int N, int *n){
     if(*n < N){
         int num;
         printf("Input Number : ");
         scanf("%d",&num);
-        List[*n] = num;
+        Array[*n] = num;
         *n += 1;
     }
     else{
@@ -31,44 +29,48 @@ void Input_List(int *List, int N, int *n){
         system("pause");
     }
 }
-void Initialization_List(int *List,int N, int *n){
+void Initialization_Array(int *Array,int N, int *n){
 	*n = 0;
 	int i;
 	for(i = 0; i < N; i++){
-		List[i] = 0;
+		Array[i] = 0;
 	}
 }
-void Max_Heapify(int *List, int i, int n){
+void Max_Heapify(int *Array, int i, int n, int N){
     int l = 2 * i + 1, r = 2 * i + 2, largest, tmp; //Left = 2 * i + 1 , Right = 2 * i + 2
-    if (l <= n && List[l]>List[i]) largest = l;
+    if (l <= n && Array[l]>Array[i]) largest = l;
     else largest = i;
-    if (r <= n && List[r] > List[largest]) largest = r;
+    if (r <= n && Array[r] > Array[largest]) largest = r;
     if (largest != i){
-        tmp = List[i];
-        List[i] = List[largest];
-        List[largest] = tmp;
-        Print_List(List,staticNUM);
+        tmp = Array[i];
+        Array[i] = Array[largest];
+        Array[largest] = tmp;
+        printf("========================================\n");
+        Print_Array(Array,N);
         printf("%d index and %d index were swapped\n",i,largest);
-        Max_Heapify(List, largest, n);
+        printf("========================================\n");
+        Max_Heapify(Array, largest, n, N);
     }
 }
-void Build_Max_Heap(int *List, int n){
+void Build_Max_Heap(int *Array, int n, int N){
     int i;
     //parent(i) = i/2
-    for( i = ((n-1)/2) ; i >= 0 ; i--) Max_Heapify(List,i,n);
+    for( i = ((n-1)/2) ; i >= 0 ; i--) Max_Heapify(Array,i,n,N);
+    printf("========================================\n");
     printf("Max Heap was builded\n");
-    Print_List(List,staticNUM);
+    Print_Array(Array,N);
+    printf("========================================\n");
 }
-void Heap_Sort(int *List, int n){
+void Heap_Sort(int *Array, int n){
     int i, tmp;
-    printf("Before Sorting : ");
-    Print_List(List,n);
-    Build_Max_Heap(List, n-1);
+    int N = n;
+    printf("HeapSort Function was called\n");
+    Build_Max_Heap(Array, n-1,N);
     for (i = n; i >=1 ; i--){
-        tmp = List[0];
-        List[0] = List[i];
-        List[i] = tmp;
-        Max_Heapify(List,0,i - 1);
+        tmp = Array[0];
+        Array[0] = Array[i];
+        Array[i] = tmp;
+        Max_Heapify(Array,0,i - 1, N);
    }
    system("pause");
 }
@@ -76,16 +78,15 @@ int main(){
     int i,n,N;
     int menu;
     int exit;
-    printf("Input size of List : ");
+    printf("Input size of Array : ");
     scanf("%d",&N);
-    int *List = malloc(sizeof(int)*N);
+    int *Array = malloc(sizeof(int)*N);
     n = 0;
     system("cls");
     while(1){
         printf("========Heap Sort Simulation========\n");
-        printf("List : ");
-        Print_List(List,n);
-        staticNUM = n;
+        printf("Array : ");
+        Print_Array(Array,n);
         printf("1) Input Number\n");
         printf("2) Initialization\n");
         printf("3) Sorting\n");
@@ -95,17 +96,17 @@ int main(){
         switch(menu){
             case 1:{
                 system("cls");
-                Input_List(List,N,&n);
+                Input_Array(Array,N,&n);
                 break;
             }
             case 2:{
                 system("cls");
-                Initialization_List(List,N,&n);
+                Initialization_Array(Array,N,&n);
                 break;
             }
             case 3:{
                 system("cls");
-                Heap_Sort(List,n-1);
+                Heap_Sort(Array,n-1);
                 break;
             }
             default:{
@@ -116,5 +117,5 @@ int main(){
         system("cls");
         if(exit == 0) break;
     }
-    free(List);
+    free(Array);
 }
